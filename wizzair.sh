d936626a427ecd7cb3ad340fe4e8eed91955ba4d
Volodymyr Shynkar <volodymyr.shynkar@gmail.com>
2018-05-10 13:54:58 +0300

    update price path with var, add auto api version definition

--- wizzair.sh
+++ wizzair.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #set -x
 
-FROM=${1:-KTW}
-TO=${2:-LWO}
+FROM=${1:-LWO}
+TO=${2:-KTW}
 DATE=${3:-2018-09-03}
 ADULT=${4:-2}
 RECEIVER=${5:-"YOUR_EMAIL@gmail.com"}
+PRICE_PATH=$(dirname "$0")/wizzair-${FROM}-${TO}.tmp
 
 # Getting new fares.
-RESULT_FULL=$(curl -s https://be.wizzair.com/7.12.1/Api/asset/farechart \
+RESULT_FULL=$(curl -s $(curl -s https://wizzair.com/static/metadata.json | jq -r .apiUrl)/asset/farechart \
 	-H 'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'\
 	-H 'content-type: application/json;charset=UTF-8'\
 	--data-binary '{
@@ -52,12 +53,12 @@ curl -s --user 'api:key-YOUR_API' \
 }
 
 # if we don't have a file, set old var as new
-if [ ! -f "wizzair.tmp" ] ; then
+if [ ! -f "${PRICE_PATH}" ] ; then
 	mail_alert
-	echo $PRICE > wizzair-${FROM}-${TO}.tmp
+	echo $PRICE > ${PRICE_PATH}
 	exit 0
 else
-	OLD_PRICE=`cat wizzair-${FROM}-${TO}.tmp`
+	OLD_PRICE=`cat ${PRICE_PATH}`
 fi
 
 # Checking is some price changed
@@ -68,4 +69,4 @@ else
 fi
 
 # Save last price to file
-echo $PRICE > wizzair-${FROM}-${TO}.tmp
+echo $PRICE > ${PRICE_PATH}
